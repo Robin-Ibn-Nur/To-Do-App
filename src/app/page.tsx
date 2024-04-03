cf7f1a5b5a931f5bda9f3d583e716ea4314110a4
saifsammy <saifsammy653@gmail.com>
2024-04-03 21:01:12 +0600

    prisma added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-
-export default function Home() {
+const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>This is Robin</h1>
+        <h1>Robin hey mera nam</h1>
         <h1>My self Robin</h1>
         <h1>I am a Programmer</h1>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
